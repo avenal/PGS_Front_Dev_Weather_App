2368c2b22c0dce088582809c4f5b639a5be212b7
avenal <avenalszarosiwy@gmail.com>
2018-12-18 15:41:35 +0100

    added temperature

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import debounce from 'lodash.debounce';
 
 
-// const apiKey = process.env.KEY;
 // const city = document.querySelector("#searchInput").value;
 // let cityTest = 'Warsaw'
 // const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityTest}&APPID=${apiKey}`;
@@ -22,6 +21,7 @@ import debounce from 'lodash.debounce';
 //
 // }
 //define UI vars
+const apiKey = process.env.KEY;
 const form = document.querySelector('#city-form');
 const cityList = document.querySelector('.collection');
 const citySearch = document.querySelector('#citySearch');
@@ -34,6 +34,30 @@ function loadEventListeners() {
   document.addEventListener('DOMContentLoaded', getCities);
   form.addEventListener('submit', addCity);
   cityList.addEventListener('click', removeCity);
+}
+
+function getWeather(city, element){
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
+  let results;
+  const p = document.createElement('p');
+
+  let procesRequest =  debounce(function () {
+    axios.get(url)
+      .then((response) => {
+        // this.results = response;
+        console.log(response.data.main.temp);
+        results = response.data.main.temp;
+        console.log(results);
+        p.innerHTML = results;
+        return element.appendChild(p);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, 400)
+
+  procesRequest();
+
 }
 
 //get cities from localStorage
@@ -53,6 +77,8 @@ function getCities(){
     li.appendChild(document.createTextNode(city));
     //create new link createElement
     const link = document.createElement('a');
+    //create new p element
+    getWeather(city, li);
     link.className = 'delete-item secondary-content';
     //add icon html
     link.innerHTML = '<i class="material-icons">delete</i>';
@@ -75,6 +101,7 @@ function addCity(e) {
     //create text node and append to li
     li.appendChild(document.createTextNode(citySearch.value));
     //create new link createElement
+    getWeather(citySearch.value, li);
     const link = document.createElement('a');
     link.className = 'delete-item secondary-content';
     //add icon html
